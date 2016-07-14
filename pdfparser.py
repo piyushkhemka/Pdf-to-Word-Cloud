899316d42d5f4d9403b4e59748ae967b861f3f2b
Piyush Khemka <piyushkhemka123@gmail.com>
2016-07-15 01:29:07 +0530

    Decrypt pdf if encrypted

--- pdfparser.py
+++ pdfparser.py
@@ -5,6 +5,8 @@ def get_string_from_pdf(pdf_path, start_page, end_page):
     content = ""
     p = open(pdf_path, 'rb')
     pdfReader = PyPDF2.PdfFileReader(p)
+    if pdfReader.isEncrypted:
+        pdfReader.decrypt('')
     #print (pdfReader.numPages)
     for pageNum in range(start_page,end_page):
         page = pdfReader.getPage(pageNum)
